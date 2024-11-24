75352845385c389ab76ebd36e18c172ae0f62536
tk <tk>
2024-11-24 22:33:27 +0100

    fix: bash script push on both repo

--- scripts/build-template.sh
+++ scripts/build-template.sh
@@ -2,14 +2,19 @@
 
 # Build the template
 
+set -euox pipefail
+
 tmp_dir="$(mktemp -d)"
 cd "$tmp_dir"
 git clone https://codeberg.org/wini/wini-template
 cd -
 cd "$tmp_dir/wini-template"
-yes | rm -rf *
+yes | rm -rf * || true
 cd -
 cp -r ./wini/* "$tmp_dir/wini-template"
 cp -r ./wini/.gitignore "$tmp_dir/wini-template"
 cd -
 gitui
+git push origin main
+git remote add github git@github.com:wini-rs/wini-template.git
+git push github main
