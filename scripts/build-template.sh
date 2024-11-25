7be9b74c5c8c5d64a964af72cd042295697ced3a
tk <tk>
2024-11-25 21:18:24 +0100

    fix: add .env in template by default

--- scripts/build-template.sh
+++ scripts/build-template.sh
@@ -13,7 +13,9 @@ yes | rm -rf * || true
 cd -
 cp -r ./wini/* "$tmp_dir/wini-template"
 cp -r ./wini/.gitignore "$tmp_dir/wini-template"
+cp -r ./wini/.env "$tmp_dir/wini-template"
 cd -
+git add -f .env
 gitui
 git push origin main
 git remote add github git@github.com:wini-rs/wini-template.git
