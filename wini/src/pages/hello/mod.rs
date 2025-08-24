755cb3ce742b6383df3233a943d63755d77010e7
tk <49250442+tkr-sh@users.noreply.github.com>
2025-08-24 15:49:38 +0200

    ➕ test: add more tests

--- wini/src/pages/hello/mod.rs
+++ wini/src/pages/hello/mod.rs
@@ -37,4 +37,31 @@ pub async fn test_meta() -> Markup {
     }
 }
 
+
+#[tokio::test]
+async fn test_meta_page() {
+    use {
+        crate::template,
+        axum::{Router, middleware::from_fn, routing::get},
+        axum_test::TestServer,
+    };
+
+    let app = Router::new()
+        .route("/meta", get(test_meta))
+        .layer(from_fn(template::template));
+    let server = TestServer::new(app).expect("creates a server");
+
+    let resp = server.get("/meta").await;
+
+    resp.assert_status_ok();
+    let txt = resp.text();
+    // Title
+    assert!(txt.contains("<title>hello world</title>"));
+    assert!(txt.contains(r#"<meta property="og:title" content="hello world">"#));
+    // Keywords
+    assert!(txt.contains(r#"<meta name="keywords" content="hello, world">"#));
+    // Other meta
+    assert!(txt.contains(r#"<meta name="world" content="hello">"#));
+    assert!(txt.contains(r#"<meta name="hello" content="world">"#));
+}
 // ENDIF
