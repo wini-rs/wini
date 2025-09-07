487a6e34bdb1b8e3fa05b4140aae6b02413dda7d
tk <49250442+tkr-sh@users.noreply.github.com>
2025-09-07 14:22:10 +0000

    🚧 wip

--- wini/src/server.rs
+++ wini/src/server.rs
@@ -2,7 +2,11 @@ use {
     crate::{
         layouts::header,
         pages,
-        shared::wini::{layer::MetaLayerBuilder, ssg::render_routes_to_files, PORT},
+        shared::wini::{
+            layer::MetaLayerBuilder,
+            ssg::{render_routes_to_files, SsgRouter},
+            PORT,
+        },
         template,
         utils::wini::{
             cache,
@@ -19,12 +23,13 @@ use {
 #[rustfmt::skip]
 // ENDIF
 pub async fn start() {
+// IFFEAT ssr
     // Support for compression
     let compression_layer = CompressionLayer::new();
 
 
     // The main router of the application is defined here
-    let app = Router::new()
+    let app = Router::<()>::new()
         .route("/", get(pages::hello::render))
         .layer(middleware::from_fn(header::render))
         .layer(
@@ -42,9 +47,43 @@ pub async fn start() {
         .route("/{*wildcard}", get(handling_file::handle_file))
         .layer(compression_layer);
 
-// IFFEAT ssg
-    render_routes_to_files().await;
+    // // Start the server
+    // info!("Starting listening on port {}...", *PORT);
+    // let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", *PORT))
+    //     .await
+    //     .expect("Couldn't start the TcpListener of the specified port.");
+    //
+    // info!("Starting the server...");
+    // axum::serve(listener, app)
+    //     .await
+    //     .expect("Couldn't start the server.");
 // ENDIF
+// IFFEAT ssg
+    // Support for compression
+    let compression_layer = CompressionLayer::new();
+
+
+    let ssg_router = SsgRouter::new()
+        .route("/", get(pages::hello::render));
+
+    // The main router of the application is defined here
+    let app = Router::<()>::new()
+        .merge(ssg_router.into_axum_router())
+        .layer(middleware::from_fn(header::render))
+        .layer(
+            MetaLayerBuilder::default()
+                .default_meta(hash_map! {
+                    "title" => "PROJECT_NAME_TO_RESOLVE".into(),
+                    "description" => "PROJECT_NAME_TO_RESOLVE".into(),
+                    "lang" => "en".into(),
+                })
+                .build()
+                .expect("Failed to build MetaLayer"),
+        )
+        .layer(middleware::from_fn(template::template))
+        .layer(middleware::from_fn(cache::html_middleware))
+        .route("/{*wildcard}", get(handling_file::handle_file))
+        .layer(compression_layer);
 
     // Start the server
     info!("Starting listening on port {}...", *PORT);
@@ -52,8 +91,13 @@ pub async fn start() {
         .await
         .expect("Couldn't start the TcpListener of the specified port.");
 
-    info!("Starting the server...");
-    axum::serve(listener, app)
-        .await
-        .expect("Couldn't start the server.");
+    tokio::task::spawn(async move {
+        info!("Starting the server...");
+        axum::serve(listener, app)
+            .await
+            .expect("Couldn't start the server.");
+    });
+
+    render_routes_to_files().await;
+// ENDIF
 }
--- wini/src/shared/wini/ssg.rs
+++ wini/src/shared/wini/ssg.rs
@@ -1,8 +1,10 @@
 use {
+    crate::shared::wini::PORT,
     axum::{handler::Handler, routing::MethodRouter, Router},
+    select::{document::Document, predicate::Name},
     std::{
+        borrow::Cow,
         collections::{HashMap, HashSet},
-        fs::create_dir_all,
         path::{Path, PathBuf},
         sync::{Arc, LazyLock, Mutex},
     },
@@ -11,7 +13,7 @@ use {
 /// A basic router for SSG
 #[derive(Debug, Default)]
 pub(crate) struct SsgRouter<'l> {
-    routes: HashMap<&'l str, MethodRouter<()>>,
+    routes: HashMap<&'l str, (MethodRouter<()>, Option<Vec<Vec<Cow<'l, str>>>>)>,
 }
 
 impl<'l> SsgRouter<'l> {
@@ -20,20 +22,30 @@ impl<'l> SsgRouter<'l> {
     }
 
     pub fn route(mut self, path: &'l str, m: MethodRouter<()>) -> Self {
-        self.routes.insert(path, m);
+        self.routes.insert(path, (m, None));
         self
     }
 
-    pub fn route_with_params(mut self, path: &'l str, m: MethodRouter<()>) -> Self {
-        self.routes.insert(path, m);
+    pub fn route_with_params(
+        mut self,
+        path: &'l str,
+        m: MethodRouter<()>,
+        params: Vec<Vec<Cow<'l, str>>>,
+    ) -> Self {
+        self.routes.insert(path, (m, Some(params)));
         self
     }
 
     pub fn into_axum_router(self) -> Router {
         let mut router = Router::new();
 
-        for (path, method_router) in self.routes {
+        for (path, (method_router, vec_params)) in self.routes {
             router = router.route(path, method_router);
+            let path_segments = string_to_path_segments(path);
+            for params in vec_params {
+                // path_segments
+            }
+            ROUTES_TO_AXUM.lock().unwrap().insert(path.to_owned());
         }
 
         router
@@ -44,15 +56,56 @@ static ROUTES_TO_AXUM: LazyLock<Arc<Mutex<HashSet<String>>>> =
     LazyLock::new(|| Arc::new(Mutex::new(HashSet::new())));
 
 pub async fn render_routes_to_files() {
-    std::fs::create_dir_all("dist/");
+    std::fs::create_dir_all("dist/").unwrap();
+    let mut static_assets = Vec::new();
+
     for route in &*ROUTES_TO_AXUM.lock().unwrap() {
-        let resp_text = reqwest::get(route).await.unwrap().text().await.unwrap();
+        let resp_text = reqwest::get(format!("http://localhost:{}{route}", *PORT))
+            .await
+            .unwrap()
+            .text()
+            .await
+            .unwrap();
+
+        let document = Document::from(resp_text.as_str());
+
+        for link in document.find(Name("link")) {
+            if let Some(href) = link.attr("href") && !href.starts_with("https://") {
+                static_assets.push(href.to_owned());
+            }
+        }
+
+        for script in document.find(Name("script")) {
+            if let Some(src) = script.attr("src") && !src.starts_with("https://") {
+                static_assets.push(src.to_owned());
+            }
+        }
+
         let mut path = PathBuf::new();
+        path.push("dist");
         path.extend(route.split('/'));
-        std::fs::create_dir_all(path.parent().unwrap());
-        std::fs::write(path, resp_text).expect("Couldn't write the file")
+        std::fs::create_dir_all(path.parent().unwrap()).unwrap();
+        path.push("index.html");
+        std::fs::write(path, resp_text).expect("Couldn't write the file");
     }
-    copy_dir_all("public", "dist");
+
+    for static_asset in static_assets {
+        let resp_text = reqwest::get(format!("http://localhost:{}{static_asset}", *PORT))
+            .await
+            .unwrap()
+            .text()
+            .await
+            .unwrap();
+
+        let mut path = PathBuf::new();
+        path.push("dist");
+        path.extend(static_asset.split('/'));
+
+        std::fs::create_dir_all(path.parent().unwrap()).unwrap();
+        std::fs::write(path, resp_text).expect("Couldn't write the file");
+    }
+
+    copy_dir_all("public", "dist").unwrap();
 }
 
 fn copy_dir_all(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> std::io::Result<()> {
@@ -79,7 +132,11 @@ fn string_to_path_segments<'l>(s: &'l str) -> Vec<PathSegment<'l>> {
     s.split('/')
         .skip(1)
         .map(|segment| {
-            match segment.chars().nth(1).expect(&format!("Invalid path: {s}")) {
+            match segment
+                .chars()
+                .nth(1)
+                .unwrap_or_else(|| panic!("Invalid path: {s}"))
+            {
                 '*' => PathSegment::Wildcard,
                 ':' | '{' => PathSegment::Param,
                 _ => PathSegment::String(segment),
